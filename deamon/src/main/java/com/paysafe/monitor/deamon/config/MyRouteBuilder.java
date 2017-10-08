80434d4d4fba4657066568348cddb76d0c733a50
Deny Lester <dimcorso@gmail.com>
2017-10-08 15:20:46 -0400

    Basic error handling

--- deamon/src/main/java/com/paysafe/monitor/deamon/config/MyRouteBuilder.java
+++ deamon/src/main/java/com/paysafe/monitor/deamon/config/MyRouteBuilder.java
@@ -1,10 +1,9 @@
 package com.paysafe.monitor.deamon.config;
 
-import java.util.List;
-
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.paysafe.monitor.core.model.Config;
 
 public class MyRouteBuilder extends RouteBuilder {
@@ -12,6 +11,26 @@ public class MyRouteBuilder extends RouteBuilder {
 	
 	@Override
 	public void configure() throws Exception {
+		
+		/*
+		 * a more advanced error handling should be plugged here. 
+		 * Log the stacktrace and send a Json response to the client
+		 * Also send the proper http status in the response.
+		 * 
+		 *  For now we put this to avoid the stack trace to be sent to the client
+		 */
+		onException(JsonProcessingException.class)
+			.handled(true)
+			.transform().simple("${exception.message}")
+			.end();
+	
+		onException(Exception.class)
+			.handled(true)
+			.transform().simple("Internal Server Error")
+			.end();
+		
+		
+		
 		from("scheduler://monitor?delay=1000")
 			.to("bean:monitorFacade?method=tick")
 		.end();
